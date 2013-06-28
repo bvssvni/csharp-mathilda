e46c08229e63567d4679e85b41a4a184824c1827
Sven Nilsen <bvssvni@gmail.com>
2013-06-29 00:30:05 +0200

    Added more tests.

--- MathildaLib/MathildaLib/Tests/TestMultiply.cs
+++ MathildaLib/MathildaLib/Tests/TestMultiply.cs
@@ -31,6 +31,30 @@ namespace MathildaLib
 
 			Assert.True (a.IsEqualTo (b));
 		}
+
+		[Test()]
+		public void TestMultiply4 () {
+			// a * 2
+			var a = new VariableNode ("a").Multiply (2);
+			Assert.True (a.ToString () == "Product (*a*2)");
+		}
+
+		[Test()]
+		public void TestMultiply5 () {
+			// a * b
+			var a = new VariableNode ("a").Multiply ("b");
+			Assert.True (a.ToString () == "Product (*a*b)");
+		}
+
+		[Test()]
+		public void TestMultiply6 () {
+			// a * (1 + 2)
+			var a = new VariableNode ("a").Multiply (
+				new NumberNode (1).Add (2));
+			var b = new VariableNode ("a").Add (
+				new NumberNode (2).Multiply ("a"));
+			Assert.True (a.IsEqualTo (b));
+		}
 	}
 }
 
